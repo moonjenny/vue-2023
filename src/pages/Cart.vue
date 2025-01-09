d6c75989764a85bd077334290c8b6778e0487c53
moon jenny <40196122+moonjenny@users.noreply.github.com>
2025-01-09 11:03:38 +0900

    장바구니 추가

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,8 +1,23 @@
+<script setup>
+  //장바구니
+  import cartFilter from './../components/cart/cartFilter.vue'
+  import cartProduct from './../components/cart/cartProduct.vue'
+  import cartPrice from './../components/cart/cartPrice.vue'
+  import cartNotice from './../components/cart/cartNotice.vue'
+</script>
+
 <template>
   <section class="cart">
-    <!-- 페이지 내용 -->
-    <h1>장바구니</h1>
-    <p>This is a new page.</p>
+    <h2 class="cart-title">장바구니</h2>
+
+    <!-- 필터 -->
+    <cartFilter />
+    <!-- 배송상품 -->
+    <cartProduct />
+    <!-- 총합계 -->
+    <cartPrice />
+    <!-- 총합계 -->
+    <cartNotice />
   </section>
 </template>
 
@@ -12,6 +27,13 @@ export default {
 }
 </script>
 
-<style scoped>
-/* 컴포넌트 스타일 */
+<style lang="scss"> 
+  .cart {
+    margin: 32px 20px 60px;
+    .cart-title {
+      font-size: 24px;
+      font-weight: 500;
+    }
+  }
+
 </style>
