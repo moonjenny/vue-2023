0cac93afe943c94df7b629860c1b5f11416a629e
uzen.lune <uzen.lune@gmail.com>
2025-06-13 02:06:14 +0000

    firebase, 1:1문의 추가

---
+++ src/pages/mypage/DirectInquiry.vue
@@ -0,0 +1,17 @@
+<script setup>
+// 1:1문의
+import { ref } from 'vue';
+
+</script>
+
+<template>
+  <section class="my-order">
+    1:1문의
+  </section>
+</template>
+
+<style lang="scss">
+/* 컴포넌트 스타일 */
+.my-order {
+}
+</style>
